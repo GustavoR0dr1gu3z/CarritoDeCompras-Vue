b8162b49843af2030d7992f117a92332294f4eb0
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2022-01-23 17:43:52 -0600

    login terminado

--- js/main.js
+++ js/main.js
@@ -18,6 +18,8 @@ const app = Vue.createApp({
         btnIniciarSesion(){
             if(this.inputName == "plerdo" && this.inputPass == "pwst1234"){
                 alert("Bienvenido\n"+this.inputName);
+            }else if(this.inputName == null || this.inputPass == null){
+                alert("Complete los campos");
             }else{
                 alert("Usuario o contraseña incorrectos");
             }
