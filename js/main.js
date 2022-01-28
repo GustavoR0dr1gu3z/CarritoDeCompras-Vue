9db5e7159f1d82978cc55b51c518a43ecb8dec27
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2022-01-28 00:04:43 -0600

    test con async await

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 const app = Vue.createApp({
 
     data(){
@@ -13,20 +14,51 @@ const app = Vue.createApp({
             
         }
     },
+    
 
     methods:{
-        btnIniciarSesion(){
-            if(this.inputName == "plerdo" && this.inputPass == "pwst1234"){
-                alert("Bienvenido\n"+this.inputName);
-            }else if(this.inputName == null || this.inputPass == null){
-                alert("Complete los campos");
+/*        async btnIniciarSesion(){
+            let resultado = await axios.post("http://prueba.pwstasp.net/api/conexion_login/login", {
+                usuario: this.loggin.inputName,
+                contrasenia: this.loggin.inputPass
+            });
+
+            if(resultado.status == 201){
+                localStorage.setItem("user-info", JSON.stringify(resultado.data));
+                this.$router.push("/home");
             }else{
-                alert("Usuario o contraseña incorrectos");
+                alert("Datos incorrectos");
+            }            
+            }
+        }*/
+
+        btnIniciarSesion(){
+            /* Async: Espera a que una promesa nos resuelva y de una data, para usarla */
+            const peticion = async () => {
+                /* Await: Espera a que una promesa nos resuelva */
+                const respuesta = await fetch("http://prueba.pwstasp.net/api/conexion_login/login") 
+                const data = await respuesta.json();                
+                return data;
             }
 
+            if (data[0].usuario == this.loggin.inputName && data[0].contrasenia == this.loggin.inputPass){
+                console.log("Datos correctos");
+            }
+
+        },
+
+        btnIniciarSesion2(){
+            const peticion = async () => {
+                const respuesta = await axios.post("http://prueba.pwstasp.net/api/conexion_login/login")
+                
+            }
         }
 
+
     }
+
+
+    
 
 
 });
