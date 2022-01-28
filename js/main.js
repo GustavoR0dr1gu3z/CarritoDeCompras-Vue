46cf3be50a67809b2417b26a3d4d8eb11ea0f281
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2022-01-28 00:57:43 -0600

    axios

--- js/main.js
+++ js/main.js
@@ -12,53 +12,21 @@ const app = Vue.createApp({
                 inputPass: "",
             }
             
-        }
+        };
     },
     
+    created(){
+        //this.btnIniciarSesion();
+    },
 
     methods:{
-/*        async btnIniciarSesion(){
-            let resultado = await axios.post("http://prueba.pwstasp.net/api/conexion_login/login", {
+        async btnIniciarSesion(){
+            const data = await axios.post("http://prueba.pwstasp.net/api/conexion_login/login",{
                 usuario: this.loggin.inputName,
                 contrasenia: this.loggin.inputPass
-            });
-
-            if(resultado.status == 201){
-                localStorage.setItem("user-info", JSON.stringify(resultado.data));
-                this.$router.push("/home");
-            }else{
-                alert("Datos incorrectos");
-            }            
-            }
-        }*/
-
-        btnIniciarSesion(){
-            /* Async: Espera a que una promesa nos resuelva y de una data, para usarla */
-            const peticion = async () => {
-                /* Await: Espera a que una promesa nos resuelva */
-                const respuesta = await fetch("http://prueba.pwstasp.net/api/conexion_login/login") 
-                const data = await respuesta.json();                
-                return data;
-            }
-
-            if (data[0].usuario == this.loggin.inputName && data[0].contrasenia == this.loggin.inputPass){
-                console.log("Datos correctos");
-            }
-
+            });                
+            console.log(data);
         },
-
-        btnIniciarSesion2(){
-            const peticion = async () => {
-                const respuesta = await axios.post("http://prueba.pwstasp.net/api/conexion_login/login")
-                
-            }
-        }
-
-
     }
-
-
-    
-
 
 });
