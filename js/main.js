5a46131c1e56333fa5a0a7d6ba2a51dae57c59d8
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2022-01-31 21:10:03 -0600

    mainjs documentado

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const app = Vue.createApp({
 
+    /* Archivo que se usa para los metodos (con API) y mensajes con VueJS*/
     data(){
         return{
             loggin:{
@@ -57,25 +58,32 @@ const app = Vue.createApp({
 
     methods:{
 
+        /* Funciones asincronas para el consumo de la API  */
         async btnIniciarSesion2(e){
             e.preventDefault();
 
+            /* Consumo del api con await por la funcion asincrona */
             let resultado = await axios.post("http://prueba.pwstasp.net/api/conexion_login/login", {
+                /* Al valor que coloquen en los input se reemplazan en los valores retornados del API */
                 usuario: this.inputName,
                 contrasenia: this.inputPass
             });
 
-
+            /* Accedemos a los atributos del API, si es verdadera la consulta, fue exitosa la consulta */
             if(resultado.data.exito == true){
                 console.log("entro con async await");
+                /* Guardamos el usuario en el localStorage para saber si hay usuarios loggeados o no */
                 localStorage.setItem("usuario", this.inputName);
 
+                /* Traemos los datos del local storage para ver si alguien tiene sesion activa */
                 us = localStorage.getItem('usuario');
 
                 if(us === "admin" ){
+                    /* Si el usuario es el admin, tiene otra pantalla diferente a los demás: CRUD de los usuarios*/
                     window.location.href = "./views/crud_users.html";
                     console.log("Bienvenido Jefe")
                 }
+                /* Si es usuario normal, le aparece el catalogo de los productos para su compra */
                 else if( us === this.inputName){
                     window.location.href = "../views/productos.html";
                     console.log("Iniciando Sesion")
@@ -84,6 +92,7 @@ const app = Vue.createApp({
                 }
 
             }else{
+                /* Fallo del logeo, datos incorrectos o faltantes */
                 console.log("no entro con async await");
                 alert(resultado.data.error)
             }
@@ -92,26 +101,34 @@ const app = Vue.createApp({
         btnCerrarSesion(e){
             e.preventDefault();
             localStorage.removeItem("usuario");
+            
+            /* Se traen datos del localStorage */
             us = localStorage.getItem('usuario');
 
+            /*Si no hay ningun dato*/            
             if( us == null){
+                /* Se regresa al index para que se loggen */
                 window.location.href = "../index.html";
                 console.log("Cerrando Sesion")
             }else{
+                /* Solo imprime quien esta logeado en la consola */
                 console.log(us);        
             }
 
         },
 
         async btnSearch(){
+            /*Consulta del API */
             let res = await axios.post("http://prueba.pwstasp.net/api/conexion_login/productos", {});
             datos = res.data.datos
             for(i=0; i<datos.length; i++){
+                /* Guarda los datos en un arreglo */
                 this.productos.datos.push(datos[i]);
             }
             console.log(datos);            
         },
 
+        /* Funcion para aumentar o disminuir en el boton de agregar/quitar */
         modCount(instruccion = "add", limit = 10){
             if(instruccion == "dis" && this.productos.counts > 0){
                 this.productos.counts -= 1;
@@ -126,6 +143,7 @@ const app = Vue.createApp({
 
         async btnGuardarUser(e){
             e.preventDefault();
+            /* Consulta del API y sustitucion de los datos */
             let resultadoGuardar = await axios.post("http://prueba.pwstasp.net/api/conexion_login/guardar_usuario", {
             fecha: this.dateG,
             nombre: this.nameG,
@@ -222,6 +240,7 @@ const app = Vue.createApp({
     },
 
     created(){
+        /* Constantemente se busca si hay alguien logeado o no para que se regrese automaticamente al login*/
         this.btnSearch();
     },
 
